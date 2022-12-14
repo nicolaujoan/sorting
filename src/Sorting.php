301b02cb451117665d83313c0f3c21135311cdbc
Joan Nicolau <joannicolau23@gmail.com>
2022-12-14 17:35:12 +0100

    finalizamos 3 algoritmos

--- src/Sorting.php
+++ src/Sorting.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace CodelyTv;
+namespace Sorting;
 
 final class Sorting
 {
@@ -25,11 +25,58 @@ final class Sorting
         return $arr;
     }
 
-    static function mergeSort($arr) {
-
+    function merge_sort($my_array)
+    {
+        if (count($my_array) == 1)
+            return $my_array;
+        $mid = count($my_array) / 2;
+        $left = array_slice($my_array, 0, $mid);
+        $right = array_slice($my_array, $mid);
+        $left = $this->merge_sort($left);
+        $right = $this->merge_sort($right);
+        return $this->merge($left, $right);
+    }
+    private function merge($left, $right)
+    {
+        $res = array();
+        while (count($left) > 0 && count($right) > 0) {
+            if ($left[0] > $right[0]) {
+                $res[] = $right[0];
+                $right = array_slice($right, 1);
+            } else {
+                $res[] = $left[0];
+                $left = array_slice($left, 1);
+            }
+        }
+        while (count($left) > 0) {
+            $res[] = $left[0];
+            $left = array_slice($left, 1);
+        }
+        while (count($right) > 0) {
+            $res[] = $right[0];
+            $right = array_slice($right, 1);
+        }
+        return $res;
     }
 
-    static function quickSort($arr) {
-        
+    function quicksort($array)
+    {
+        if (count($array) < 2)
+            return $array;
+
+        $left = $right = array();
+
+        reset($array);
+        $pivot_key = key($array);
+        $pivot = array_shift($array);
+
+        foreach ($array as $k => $v) {
+            if ($v < $pivot)
+                $left[$k] = $v;
+            else
+                $right[$k] = $v;
+        }
+
+        return array_merge($this->quicksort($left), array($pivot_key => $pivot), $this->quicksort($right));
     }
 }
--- tests/SortingTest.php
+++ tests/SortingTest.php
@@ -6,15 +6,39 @@ namespace Sorting\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use CodelyTv\Sorting;
+use Sorting\Sorting;
 
 final class SortingTest extends TestCase
 {
     /** @test */
-    public function itShouldSortFromSmallToBig(): void
+    public function itShouldSortFromSmallToBigWithBubbleSort(): void
     {
         $unorderedArray = [5, 1, 4, 2, 8];
 
         self::assertEquals([1, 2, 4, 5, 8], Sorting::bubbleSort($unorderedArray));
+    }
+
+    /** @test */
+    public function itShouldSortFromSmallToBigWithMergeSort(): void
+    {
+        $unorderedArray = [8, 21, 654, 20, 10, 0, -23, -2, 45, 1, 34];
+
+        $sorting = new Sorting();
+
+        self::assertEquals(
+            [-23, -2, 0, 1, 8, 10, 20, 21, 34, 45, 654],
+            $sorting->merge_sort($unorderedArray)
+        );
+    }
+
+    /** @test */
+    public function itShouldSortFromSmallToBigWithQuickSort()
+    {
+
+        $unorderedArray = [5, 4, 3, 2, 1];
+
+        $sorting = new Sorting();
+
+        self::assertEquals([1, 2, 3, 4, 5], $sorting->quickSort($unorderedArray));
     }
 }
